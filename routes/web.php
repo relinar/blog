e9fd6e1d930554171c5327a98ddadee700876072
Relina Russak <relinarussak@gmail.com>
2025-11-18 11:51:45 +0200

    Add posts

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
+
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PublicController::class, 'index']);
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
